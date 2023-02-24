2ca0ca9cede8cbfaae3f8472c52cc44070f7cfb0
Nicolas Riquelme <nrriquelme@gmail.com>
2023-02-24 20:32:44 -0300

    testing minting position

---
+++ test/ERC20Mintable.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.14;
+
+import "solmate/tokens/ERC20.sol";
+
+contract ERC20Mintable is ERC20 {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol, _decimals) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -2,11 +2,102 @@
 pragma solidity ^0.8.14;
 
 import "forge-std/Test.sol";
+import "./ERC20Mintable.sol";
+import "../src/UniswapV3Pool.sol";
 
 contract UniswapV3PoolTest is Test {
-  function setUp() public {}
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+    UniswapV3Pool poo;
+    bool shouldTransferInCallback = false;
 
-  function testExample() public {
-    assertTrue(true);
-  }
+    struct TestCaseParams {
+        uint256 wethBalance;
+        uint256 usdcBalance;
+        int24 currentTick;
+        int24 lowerTick;
+        int24 upperTick;
+        uint128 liquidity;
+        uint160 currentSqrtP;
+        bool shouldTransferInCallback;
+        bool mintLiqudity;
+    }
+
+    function setUp() public {
+        token0 = new ERC20Mintable("Ether", "ETH", 18);
+        token1 = new ERC20Mintable("USDC", "USDC", 18);
+    }
+
+    function testExample() public {
+        assertTrue(true);
+    }
+
+    function TestMintSuccess() public {
+      TestCaseParams memory params = TestCaseParams({
+        wethBalance: 1 ether,
+        usdcBalance: 5000 ether,
+        currentTick: 85176,
+        lowertick: 84222,
+        upperTick: 86129,
+        liquidity: 1517882343751509868544,
+        currentSqrtP: 5602277097478614198912276234240,
+        shouldTransferInCallback: true,
+        mintLiqudity: true
+      })
+
+      (uint256 poolBalance0, uint256 poolBalance1) = setupTestCase(params);
+
+      uint256 expectedAmount0 = 0.99897661834742528 ether;
+      uint256 expectedAmount1 = 5000 ether;
+
+      assertEq(
+        poolBalance0,
+        expectedAmount0,
+        "incorrect token0 deposited amount"
+      );
+
+      assertEq(
+        poolBalance1,
+        expectedAmount1,
+        "incorrect token1 deposited amount"
+      );
+
+      assertEq(
+        token0.balanceOf(address(pool)), expectedAmount0
+      )
+
+      assertEq(
+        token1.balaceOf(address(pool)), expectedAmount1
+      )
+    }
+
+    function setupTestCase(TestCaseParams memory params) internal returns (
+      uint256 poolBalance0, uint256 poolBalance1
+    ) {
+      token0.mint(address(this), params.wethBalance);
+      token1.mint(address(this), params.usdcBalance);
+
+      pool = new UniswapV3Pool(
+        address(token0),
+        address(token1),
+        params.currentSqrtP,
+        params.currentTick
+      );
+
+      if (params.mintLiqudity){
+        (poolBalance0, poolBalance1) = pool.mint(
+          address(this),
+          params.lowerTick,
+          params.upperTick,
+          params.liquidity
+        )
+      }
+
+      shouldTransferInCallback = params.shouldTransferInCallback;
+    }
+
+    function uniswapV3MintCallback(uint256 amount0, uint256 amount1) public {
+      token0.transfer(msg.sender, amount0);
+      token1.transfer(msg.sender, amount1);
+    }
 }
